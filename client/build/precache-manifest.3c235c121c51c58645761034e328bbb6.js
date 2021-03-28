cdd6d51ab61ea9b301171d612b7471e3264efb3b
Justyna Żurawiecka <justyna2106@wp.pl>
2021-03-28 17:39:56 +0200

    tests -publish app on heroku

---
+++ client/build/precache-manifest.3c235c121c51c58645761034e328bbb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92ee39c1d032d86b059d343d9178438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "504cc31dd122de5e751d",
+    "url": "/static/css/2.c724ebb2.chunk.css"
+  },
+  {
+    "revision": "65c459d261afae30513c",
+    "url": "/static/css/main.c3197eef.chunk.css"
+  },
+  {
+    "revision": "504cc31dd122de5e751d",
+    "url": "/static/js/2.6c97eb8c.chunk.js"
+  },
+  {
+    "revision": "65c459d261afae30513c",
+    "url": "/static/js/main.3bcfb206.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
